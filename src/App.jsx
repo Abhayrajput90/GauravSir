c6c6081848ecafc1ce04ae4d529665e4642bef1f
= <=>
2024-05-29 11:22:05 +0530

    GauravSir

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './Coponents/header'
+import Home from './Coponents/home'
+import Footer from './Coponents/footer'
+import Spinner from './Coponents/spinner'
+
+function App() {
+  return (
+    <>
+    <Spinner />
+    <Header />
+    <Home />
+    <Footer />
+    </>
+    
+  )
+}
+
+export default App
